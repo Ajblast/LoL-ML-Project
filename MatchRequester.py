b6d75b205322f0216caaa0e0a331391bfe1ac442
Ajblast <46971018+Ajblast@users.noreply.github.com>
2022-04-09 06:09:02 -0400

    Add QueueType

--- MatchRequester.py
+++ MatchRequester.py
@@ -1,16 +1,19 @@
-from RIOTAPI import RIOTAPI, MajorRegion
+from RIOTAPI import RIOTAPI, MajorRegion, QueueType
 
 class MatchRequester:
     def __init__(self, api : RIOTAPI):
         self.api = api
 
-    def RequestMatchIds(self, region : MajorRegion, puuid : str, startIndex : int, count : int):
+    def RequestMatchIds(self, region : MajorRegion, puuid : str, startIndex : int, count : int, queueType : QueueType = None):
         if (count <= 0 or count > 100):
             raise ValueError("count must be at least 1 and less than or equal to 100")
         if (startIndex < 0):
             raise ValueError("startIndex must be at least 0")
 
-        endpoint = "/lol/match/v5/matches/by-puuid/{}/ids?start={}&count={}".format(puuid, startIndex, count)
+        if (queueType is None):
+            endpoint = "/lol/match/v5/matches/by-puuid/{}/ids?&start={}&count={}".format(puuid, startIndex, count)
+        else:
+            endpoint = "/lol/match/v5/matches/by-puuid/{}/ids?queue={}&start={}&count={}".format(puuid, queueType.value, startIndex, count)
 
         return self.api.requestMajorRegion(region, endpoint)
     
