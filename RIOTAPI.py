1cf20be6a786e3e788bb652df2ec1102df194dc2
Ajblast <46971018+Ajblast@users.noreply.github.com>
2022-04-09 03:14:00 -0400

    Add API rate limits

--- RIOTAPI.py
+++ RIOTAPI.py
@@ -1,13 +1,22 @@
 import requests
-import json
+import RateLimit
 
 class RIOTAPI:
     def __init__(self, apikey):
         self.key = apikey
+        self.secondRateLimit = RateLimit(20, 1, 0.1)
+        self.minuteRateLimit = RateLimit(100, 120, 0.1)
+
 
     def request(self, endpoint):
         headers = {"X-Riot-Token": self.key}
         url = "https://na1.api.riotgames.com" + endpoint
+
+        # Wait for the second rate limit
+        self.secondRateLimit.waitForRateLimit()
+        # Wair for the minute rate limit
+        self.minuteRateLimit.waitForRateLimit()
+
 
         r = requests.get(url, headers=headers)
         return r.json()
---
+++ RateLimit.py
@@ -0,0 +1,32 @@
+import time
+
+class RateLimit:
+    #Rate is how many requests can be done in the time frame (In Seconds)
+    def __init__(self, rate, timeFrame, timeEpsilon, sleepTime):
+        self.rate = rate
+        self.timeFrame = timeFrame
+        self.timeEpsilon = timeEpsilon
+        self.sleepTime = sleepTime
+        self.requests = 0
+        self.startTime = time.perf_counter()
+
+    # Wait for the rate limit to be valid
+    def waitForRateLimit(self):
+        # Reset the rate limit if the timeframe has passed since starting
+        if (self._DeltaTime() >= self.timeFrame):
+            self._reset()
+
+        # Increase request count
+        self.requests += 1
+
+        # If the request count equals the rate, wait for the passedTime + timeEpsilon to reach the time frame
+        if (self.requests == self.rate):
+            while (self._DeltaTime() + self.timeEpsilon < self.timeFrame):
+                time.sleep(self.sleepTime)
+
+    def _reset(self):
+        self.requests = 0
+        self.startTime = time.perf_counter()
+
+    def _DeltaTime(self):
+        return time.perf_counter() - self.startTime()
