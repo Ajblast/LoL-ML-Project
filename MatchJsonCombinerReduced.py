f2db5ae9e9f43711fdd6476ddb17ffc87d80c68a
Seth Bowden <79893142+bowdens07@users.noreply.github.com>
2022-04-24 23:47:05 -0400

    Add Reduced Model
    Add reduced model

---
+++ MatchJsonCombinerReduced.py
@@ -0,0 +1,30 @@
+import json
+
+#match0 = open("Matches0.json", "r")
+#match1 = open("Matches1.json", "r")
+match2 = open("Matches2.json", "r")
+match3 = open("Matches3.json", "r")
+
+#print("Load Match 0")
+#match0json = json.load(match0)
+#print("Load Match 1")
+#match1json = json.load(match1)
+print("Load Match 2")
+match2json = json.load(match2)
+print("Load Match 3")
+match3json = json.load(match3)
+
+#match0.close()
+#match1.close()
+match2.close()
+match3.close()
+
+matches = []
+#matches.extend(match0json)
+#matches.extend(match1json)
+matches.extend(match2json)
+matches.extend(match3json)
+
+print("Write Matches")
+with open("Matches2-3.json", "w") as outfile:
+    json.dump(matches, outfile, separators=(',', ':'))
---
+++ ModelTrainingReduced.py
@@ -0,0 +1,76 @@
+import progressbar
+import json
+import pandas as pd
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import LSTMModel
+
+#Load the matches
+matches = None
+with open("ReducedMatchesProcessed2-3.json", "r") as infile:
+    matches = json.load(infile)
+
+#Get the match data frames
+matchDataFrames = []
+for matchid in matches:
+    match = matches[matchid]
+    winningTeam = match['winningteam']
+    frames = match['frames']
+
+    matchDataFrame = pd.DataFrame(frames).T
+    matchDataFrame['winningteam'] = winningTeam
+
+    matchDataFrames.append(matchDataFrame)
+del matches
+
+#Get the training and test set
+indices = range(len(matchDataFrames))
+trainset = random.sample(indices, k = int(len(indices) * .8))
+testset = [x for x in indices if x not in trainset]
+
+trainset = [matchDataFrames[i] for i in trainset]
+testset = [matchDataFrames[i] for i in testset]
+
+
+#Create the LSTM model
+model = LSTMModel.LSTMModel(2, 102, 32, 2)
+loss_function = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+totalloss = []
+
+#Train
+widgets = ['[', progressbar.Counter(format='%(value)d/%(max_value)d'), ']', progressbar.Timer(format="Elapsed Time: %(elapsed)s"), ']', progressbar.Bar('*')]
+for i in progressbar.progressbar(range(len(trainset)), widgets=widgets):
+    match = trainset[i]
+
+    # Step 1. Remember that Pytorch accumulates gradients.
+    # We need to clear them out before each instance
+    model.zero_grad()
+
+    # Step 2. Get our inputs ready for the network, that is, turn them into Tensors.
+    x = match.loc[:, match.columns != 'winningteam']
+    y = match.loc[:, 'winningteam'].replace([100, 200], [0, 1])
+
+    x = torch.tensor(x.values, dtype=torch.float32)
+    y = torch.tensor(y.values, dtype=torch.long)
+
+    # Step 3. Run our forward pass.
+    output = model(x)
+
+    # Step 4. Compute the loss, gradients, and update the parameters by calling optimizer.step()
+    loss = loss_function(output, y)
+    totalloss.append(loss.item())
+    loss.backward()
+    optimizer.step()
+
+    if i % int(len(trainset) * .1) == 0:
+        print(f"\nEpoch {i}, Loss: {np.average(np.asarray(totalloss)):.2f}")
+
+
+torch.save(model, "TestModel.pt")
+
